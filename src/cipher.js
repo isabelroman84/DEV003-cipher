6dcfb5160a1770491dc69db8445180bae74064d0
unknown <isabelcristinaroman@gmail.com>
2022-12-15 17:36:50 -0500

    Primera versión

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,34 @@
 const cipher = {
-  // ...
-};
+  //van los métodos del objeto cipher
+  encode: function(offset, string) {
+    
+    if(typeof offset !== 'number' || typeof string !== 'string') {
+      throw new TypeError();
+    } 
+
+    const textoCodificado = '';
+    const offsetNumber = parseInt(offset);
+    
+    //recorrer variable donde está guardado el mensaje
+    for(let i = 0; i < string.length; i++) {
+      const code = string.charCodeAt(i);
+      const message = string[i];
+      //transformar letra a codigo ASCII con método JS
+      if(code >= 65 && code <= 90) {
+      //return (((code - 65) + offsetNumber) % 26) + 65;
+        //return textoCodificado += String.fromCharCode(message);
+        //transformar letra ASCCI a alfabeto ABC
+        //String.charcode
+        //textoCodificado += message;    
+      } 
+      //return textoCodificado;
+      console.log(message, code);  
+    }
+  }
+} 
+
+//Quiero definir una excepción (throw) de que los datos que ingresen sean letras y números donde corresponde
+//entonces si ingresan algo !== arrojará error. El new se usa para invocar una función y crear un objeto que es el TypeError
+    
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+//alert('hola');
 import cipher from './cipher.js';
+//declarar variable para almacenar funcionalidad en botón cifrar, asociado con valor del input ingresado por el usuario
+const btnEncriptar = document.getElementById('cifrar');
+btnEncriptar.addEventListener('click', encriptarMensaje); //parámetros: cuál evento y en qué variable
+//tomar valor del input y guardar en una variable el mensaje para que al hacer click tome ese valor
+function encriptarMensaje() {
+  const offset = parseInt(document.getElementById('offset').value); //value me muestra lo que el usuario ingresó
+  const mensaje = document.getElementById('mensaje').value;
+  const mensajeFin = document.getElementById('mensajeFin');
+  msjForEncript = cipher.encode(offset, mensaje.toUpperCase()); //almacené el método del objeto cipher en una variable
+
+  msjForEncript = mensajeFin.value;
+  
+  //console.log(typeof(encriptarMensaje));
+}
 
 console.log(cipher);
