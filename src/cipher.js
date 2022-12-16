e7207c3f8195d731fea5174c7b47f6c5ea8159d6
unknown <isabelcristinaroman@gmail.com>
2022-12-16 17:57:41 -0500

    cipher.encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,34 +1,51 @@
 const cipher = {
   //van los métodos del objeto cipher
-  encode: function(offset, string) {
-    
-    if(typeof offset !== 'number' || typeof string !== 'string') {
+  encode: function (offset, string) {
+    if (typeof offset !== 'number' || typeof string !== 'string') {
       throw new TypeError();
-    } 
-
-    const textoCodificado = '';
-    const offsetNumber = parseInt(offset);
-    
-    //recorrer variable donde está guardado el mensaje
-    for(let i = 0; i < string.length; i++) {
-      const code = string.charCodeAt(i);
-      const message = string[i];
-      //transformar letra a codigo ASCII con método JS
-      if(code >= 65 && code <= 90) {
-      //return (((code - 65) + offsetNumber) % 26) + 65;
-        //return textoCodificado += String.fromCharCode(message);
-        //transformar letra ASCCI a alfabeto ABC
-        //String.charcode
-        //textoCodificado += message;    
-      } 
-      //return textoCodificado;
-      console.log(message, code);  
     }
-  }
-} 
 
-//Quiero definir una excepción (throw) de que los datos que ingresen sean letras y números donde corresponde
-//entonces si ingresan algo !== arrojará error. El new se usa para invocar una función y crear un objeto que es el TypeError
-    
+    let textoCodificado = ''; //devuelve el resultado de la función
+    let code = '';
+    let newCode = '';
+    //console.log(string);
+
+    //recorrer variable donde está guardado el mensaje
+    for (let i = 0; i < string.length; i++) {
+      code = string.charCodeAt(i);
+      //console.log(code);
+      //transformar letra a codigo ASCII con método JS
+      if (code >= 65 && code <= 90) {
+        //transformar letra ASCCI a alfabeto ABC
+        newCode = (code - 65 + offset) % 26 + 65;
+        //console.log(newCode);
+      } else if (code === 32) {
+        newCode = 32;
+      }
+      textoCodificado += String.fromCharCode(newCode); //así se van acumulando los caracteres de la cadena
+    }
+    return textoCodificado;
+  }
+}
+//console.log(cipher.encode(3, 'ABC'));
+
+//Hasta este punto me funciona la función codificar, cuando intento agregar algo más, se desactiva
+//el keyup de las mayúsculas automáticas y ya no codifica. Por eso no pude hacer la parte del encode
+// encode: function(offset, string) {
+//let textoCodificado2 = '';
+//let code2 = '';
+//let newCode2 = '';
+//for (let i = 0; i < string.length; i++) {
+//code2 = string.charCodeAt(i);
+//if (code >= 65 && code <= 90) {
+//newCode2 += String.fromCharCode((code + 65 - offset) % 26 - 65);
+//} else if (code === 32) {
+//newCode2 = 32;
+//}
+//textoCodificado2 += String.fromCharCode(newCode2);
+//}
+//return textoCodificado;
+//}
+//}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 //alert('hola');
 import cipher from './cipher.js';
+
+document.getElementById('mensaje').addEventListener('keyup', mayus);
+function mayus() {
+  const mayusc = document.getElementById('mensaje');
+  mayusc.value = mayusc.value.toUpperCase();
+}
+
 //declarar variable para almacenar funcionalidad en botón cifrar, asociado con valor del input ingresado por el usuario
 const btnEncriptar = document.getElementById('cifrar');
 btnEncriptar.addEventListener('click', encriptarMensaje); //parámetros: cuál evento y en qué variable
@@ -8,11 +15,18 @@ function encriptarMensaje() {
   const offset = parseInt(document.getElementById('offset').value); //value me muestra lo que el usuario ingresó
   const mensaje = document.getElementById('mensaje').value;
   const mensajeFin = document.getElementById('mensajeFin');
-  msjForEncript = cipher.encode(offset, mensaje.toUpperCase()); //almacené el método del objeto cipher en una variable
+  mensajeFin.value = cipher.encode(offset, mensaje); //asigné el valor del cipher.encode al mensaje codificado
 
-  msjForEncript = mensajeFin.value;
-  
-  //console.log(typeof(encriptarMensaje));
+}
+const btnDesencriptar = document.getElementById('descifrar');
+btnDesencriptar.addEventListener('click', desencriptarMensaje);
+
+function desencriptarMensaje() {
+  const offset2 = parseInt(document.getElementById('offset').value);
+  const mensaje2 = document.getElementById('mensaje').value;
+  const mensajeFin2 = document.getElementById('mensajeFin');
+  mensajeFin.value = cipher.decode(offset2, mensaje2);
 }
 
+//console.log(typeof(desencriptarMensaje));
 console.log(cipher);
