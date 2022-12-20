7b0d6649c04b5e50449f2399ead0ee1c20212b2f
Isabel Román <isabelcristinaroman@gmail.com>
2022-12-20 17:03:07 -0500

    Updates

--- cipher.js
+++ cipher.js
@@ -8,8 +8,7 @@ const cipher = {
     let textoCodificado = ''; //devuelve el resultado de la función
     let code = '';
     let newCode = '';
-    //console.log(string);
-
+   
     //recorrer variable donde está guardado el mensaje
     for (let i = 0; i < message.length; i++) {
       code = message.charCodeAt(i);
@@ -37,7 +36,7 @@ const cipher = {
       code = message.charCodeAt(i);
       if (code >= 65 && code <= 90) {
         newCode = ((code + 65 - offset) % 26 + 65);
-      } else if (code < 65 || code > 90) {
+      } if (code < 65 || code > 90) {
         newCode = 0 + code;
       }
       textoDecodificado += String.fromCharCode(newCode);
